d5c9463b275b419d57a047fa0186b5c3f8b158f6
TyostoKarry <114697841+TyostoKarry@users.noreply.github.com>
2025-01-18 14:07:28 +0200

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,34 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintImport from 'eslint-plugin-import'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      react.configs.recommended,
+      'plugin:import/recommended',
+      'plugin:import/typescript',
+      'plugin:prettier/recommended'
+    ],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
+      'react': react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'import': eslintImport,
+      'prettier': prettier
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +36,16 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'import/order': [
+        'warn',
+        {
+          'groups': ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+          'newlines-between': 'always',
+          'alphabetize': { 'order': 'asc', 'caseInsensitive': true }
+        }
+      ],
+      'import/no-duplicates': 'warn',
+      'prettier/prettier': 'error',
     },
   },
 )
