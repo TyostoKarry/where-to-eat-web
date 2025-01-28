3c01f3690e7d9be6a9030d9dd972a4590d5403f2
TyostoKarry <114697841+TyostoKarry@users.noreply.github.com>
2025-01-28 20:20:39 +0200

    Fix RestaurantCard story not having correct parameters

--- src/stories/RestaurantCard.stories.ts
+++ src/stories/RestaurantCard.stories.ts
@@ -7,7 +7,9 @@ const Meta: Meta<typeof RestaurantCard> = {
   tags: ["autodocs"],
   args: {
     restaurantName: "Restaurant Name",
-    distance: "1.2 km",
+    distance: 123,
+    latitude: 12.345,
+    longitude: 12.345,
   },
 };
 
@@ -19,9 +21,10 @@ export const NoProps: Story = {};
 export const WithProps: Story = {
   args: {
     address: "Restaurant address 12 A 3",
+    postalCode: "12345",
     cuisine: ["Italian", "Pizza"],
     dietaryOptions: ["Vegan", "Vegetarian"],
-    openingHours: "Mon-Sun 12:00-22:00",
+    openingHours: "Mo-Fr 12:00-22:00;Sa-Su 12:00-23:00",
     phoneNumber: "+1234567890",
     website: "https://www.example.com",
   },
