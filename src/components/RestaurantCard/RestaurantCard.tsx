24c21665392d4cce0eee4ec767784de34e59f44c
TyostoKarry <114697841+TyostoKarry@users.noreply.github.com>
2025-06-08 21:41:52 +0300

    Remove surplus second scrollbar from main-content

--- src/components/RestaurantCard/RestaurantCard.tsx
+++ src/components/RestaurantCard/RestaurantCard.tsx
@@ -41,12 +41,12 @@ export const RestaurantCard: FC<RestaurantCardProps> = ({
   const toggleMap = () => {
     const selection = window.getSelection()?.toString();
     // If text is selected, do not toggle the map
-    if (selection && selection.toString().length > 0) {
+    if (selection && selection.length > 0) {
       return;
     }
 
     setShowMap((prev) => !prev);
-    setTimeout(() => updateLayout(), 10);
+    requestAnimationFrame(() => updateLayout());
   };
 
   return (
